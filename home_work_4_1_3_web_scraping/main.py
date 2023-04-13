408d924fdd2c9f452306f582eb3c660f9697d675
Khaidarov Denis <denapril20@gmail.com>
2023-04-13 23:34:29 +0300

    web scraping small refactoring

--- home_work_4_1_3_web_scraping/main.py
+++ home_work_4_1_3_web_scraping/main.py
@@ -2,68 +2,79 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-vacancies = []
 
-def get_vacancies():
-    i = 0
-    url = f"https://spb.hh.ru/search/vacancy?text=python&area=1&area=2&page={i}"
+def fetch_vacancies_page(page_number):
+    url = f"https://spb.hh.ru/search/vacancy?text=python&area=1&area=2&page={page_number}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
     }
-    response = requests.get(url, headers=headers)
-    while response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        main_content = soup.find('main', {'class', 'vacancy-serp-content'})
-        get_info_in_vacancy(main_content, vacancies)
-        i = i + 1
-        url = f"https://spb.hh.ru/search/vacancy?text=python&area=1&area=2&page={i}"
-        response = requests.get(url, headers=headers)
-    write_to_json(vacancies)
-    return vacancies
+    return requests.get(url, headers=headers)
 
 
-def get_info_in_vacancy(main_content, vacancies):
-    for vacancy in main_content.find_all('div', {'class': 'serp-item'}):
-            item_body__main_info = vacancy.find('div', {'class': 'vacancy-serp-item-body__main-info'})
-            item_title = item_body__main_info.find('a', {'class': 'serp-item__title'}).text
-            item_href = item_body__main_info.find('a', {'class': 'serp-item__title'})['href']
-            item_salary = get_salary(item_body__main_info)
-            item_company = get_company(item_body__main_info)
-            item_city = get_city(item_body__main_info)
-            append_vacancies(item_title, item_href, item_salary, item_company, item_city, vacancies)
-            
+def extract_vacancies(soup):
+    main_content = soup.find('main', {'class', 'vacancy-serp-content'})
+    return main_content.find_all('div', {'class': 'serp-item'})
 
 
-def append_vacancies(item_title, item_href, item_salary, item_company, item_city, vacancies):
-    if 'django' in item_title.lower() or 'flask' in item_title.lower():
-        my_dict = {item_title: (item_href, item_salary, item_company, item_city)}
-        vacancies.append(my_dict)
+def extract_vacancy_info(vacancy):
+    info = vacancy.find('div', {'class': 'vacancy-serp-item-body__main-info'})
+    title = info.find('a', {'class': 'serp-item__title'}).text
+    href = info.find('a', {'class': 'serp-item__title'})['href']
+    salary = extract_salary(info)
+    company = extract_company(info)
+    city = extract_city(info)
+    return title, href, salary, company, city
 
 
-def get_salary(item_body__main_info):
-    item_salary_info = item_body__main_info.find('span', class_='bloko-header-section-3')
-    if item_salary_info is None:
+def extract_salary(info):
+    salary_info = info.find('span', class_='bloko-header-section-3')
+    if salary_info is None:
         return 'Зарплата не указана'
     else:
-        salary_text = ' '.join(item_salary_info.stripped_strings).replace('\u202f', ' ')
-        return salary_text
+        return ' '.join(salary_info.stripped_strings).replace('\u202f', ' ')
 
 
-def get_city(item_body__main_info):
-    vacancy_info = item_body__main_info.find('div', {'class': 'vacancy-serp-item__info'})
-    city = vacancy_info.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text.split()[0].rstrip(',.')
-    return city
+def extract_city(info):
+    vacancy_info = info.find('div', {'class': 'vacancy-serp-item__info'})
+    return vacancy_info.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text.split()[0].rstrip(',.')
 
 
-def get_company(item_body__main_info):
-    vacancy_info = item_body__main_info.find('div', {'class': 'vacancy-serp-item__info'})
-    company_name = vacancy_info.find('a', {'class': 'bloko-link_kind-tertiary'}).text.replace('\xa0', ' ')
-    return company_name
+def extract_company(info):
+    vacancy_info = info.find('div', {'class': 'vacancy-serp-item__info'})
+    return vacancy_info.find('a', {'class': 'bloko-link_kind-tertiary'}).text.replace('\xa0', ' ')
 
 
-def write_to_json(vacancies):
+def append_vacancy(vacancies, title, href, salary, company, city):
+    if 'django' in title.lower() or 'flask' in title.lower():
+        vacancies.append({title: (href, salary, company, city)})
+
+
+def write_vacancies_to_json(vacancies):
     with open('vacancies.json', 'w', encoding='utf-8') as f:
         json.dump(vacancies, f, ensure_ascii=False, indent=4)
+
+
+def get_vacancies():
+    vacancies = []
+    page_number = 0
+
+    while True:
+        response = fetch_vacancies_page(page_number)
+
+        if response.status_code != 200:
+            break
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        vacancies_list = extract_vacancies(soup)
+
+        for vacancy in vacancies_list:
+            title, href, salary, company, city = extract_vacancy_info(vacancy)
+            append_vacancy(vacancies, title, href, salary, company, city)
+
+        page_number += 1
+
+    write_vacancies_to_json(vacancies)
+    return vacancies
 
 
 print(get_vacancies())
